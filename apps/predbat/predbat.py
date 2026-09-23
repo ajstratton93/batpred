26889467d078a9b5c173703788bba5b3210531d1
Andrew Stratton <andrew.j.stratton@gmail.com>
2026-09-23 13:08:21 +0100

    update to version v26.9.23.1

--- apps/predbat/predbat.py
+++ apps/predbat/predbat.py
@@ -34,7 +34,8 @@ import hass as hass
 import pytz
 import asyncio
 
-THIS_VERSION = "v26.9.23.0"
+THIS_VERSION = "v26.9.23.1"
+THIS_VERSION_DISPLAY = THIS_VERSION
 
 # fmt: off
 PREDBAT_FILES = ["predbat.py", "hass.py", "config.py", "prediction.py", "gecloud.py", "utils.py", "inverter.py", "ha.py", "download.py", "web.py", "web_helper.py", "predheat.py", "futurerate.py", "octopus.py", "solcast.py", "execute.py", "plan.py", "fetch.py", "output.py", "userinterface.py", "energydataservice.py", "alertfeed.py", "compare.py", "db_manager.py", "db_engine.py", "plugin_system.py", "ohme.py", "components.py", "fox.py", "carbon.py", "web_mcp.py", "component_base.py"]
